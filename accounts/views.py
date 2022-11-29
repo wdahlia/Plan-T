4269bb5acfdca57adc5b4aa07c320f2841ee479b
qlghwp123 <qlghwp123@naver.com>
2022-11-29 15:15:38 +0900

    feature : Add accounts/signup func 회원가입 기능 추가 및 테스트용 html 추가 성, 이름 필드 제거 CustomUserCreation class 추가 url 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # 바꿔야 함!
+            return redirect("accounts:signup")
+
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        "form": form,
+    }
+
+    # 바꿔야 함!
+    return render(request, "test/signup.html", context)
