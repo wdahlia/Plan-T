b2cbb1a4c97f407d49de6ac7551d056c1ff51d60
qlghwp123 <qlghwp123@naver.com>
2022-11-29 15:29:44 +0900

    feature : Add accounts/login func 로그인 기능과 url 추가 테스트 url, 기능 그리고 페이지 추가 회원가입, 로그인 페이지 이름 통일(form.html)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path("test/", views.test, name="test"),
     path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
 ]
