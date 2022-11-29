8cf22958448d734aa3d959c1a576416e44d0b20f
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-29 11:12:31 +0900

    feature : todos/model - Todos, Timetable 모델 추가

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
+class Todos(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        processors=[ResizeToFill(1200, 960)],
+        format="JPEG",
+        options={"quality": 80},
+    )
+    started_at = models.DateTimeField(blank=True)
+    expired_at = models.DateTimeField(blank=True)
+
+
+class Timetable(models.Model):
+    todo_id = models.ForeignKey("Todos", on_delete=models.CASCADE)
+    today = models.DateTimeField(auto_now_add=True)
+    start = models.IntegerField()
+    end = models.IntegerField()
