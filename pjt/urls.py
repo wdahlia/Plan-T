160833b976bbab1f1d83108732b4fb9389fa461f
chaehyun_oh <ohchaehh@gmail.com>
2022-11-30 10:44:22 +0900

    templates: accounts_form.html 생성, 로그인, 가입 기능 연결

--- pjt/urls.py
+++ pjt/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
     path("todos/", include("todos.urls")),
     path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
