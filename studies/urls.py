37e4a0caa0363f3944636ac5b8b527bd34a3b817
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-12-06 15:47:26 +0900

    feature : studies/detail
    - 가입과 수락 기능 잘 되나 확인을 위해 디테일 페이지 생성
    - 문제가 없어 보여서 푸쉬

--- studies/urls.py
+++ studies/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("create/<int:study_pk>", views.create_todos, name="create_todos"),
+    path("detail/<int:study_pk>", views.detail, name="detail"),
     path("detail/<int:study_pk>/join", views.join, name="join"),
+    path("detail/<int:study_pk>/<int:user_pk>/accept", views.accept, name="accept"),
 ]
