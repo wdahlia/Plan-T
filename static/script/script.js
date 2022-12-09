45f8f437302777cf958a31911ae9e4fe89a354fe
chaehyun_oh <ohchaehh@gmail.com>
2022-12-09 17:35:15 +0900

    templates: main_index 슬라이드 추가

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,80 @@
 try {
+
+    const slider = function () {
+        const slides = document.querySelectorAll('.slide');
+        const btnLeft = document.querySelector('.slider-btn-left');
+        const btnRight = document.querySelector('.slider-btn-right');
+        const dotContainer = document.querySelector('.dots');
+
+        let curSlide = 0;
+        const maxSlide = slides.length;
+
+        const createDots = function () {
+            slides.forEach(function (_, i) {
+                dotContainer.insertAdjacentHTML('beforeend', `<button class="dots-dot" data-slide="${i}"></button>`);
+            })
+        };
+        const activeDot = function (slide) {
+            document.querySelectorAll('.dots-dot').forEach(dot => dot.classList.remove('dots-dot-active'));
+            document.querySelector(`.dots-dot[data-slide="${slide}"]`).classList.add('dots-dot-active');
+        };
+
+        const goToSlide = function (slide) {
+            slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+        }
+
+        const nextSlide = function () {
+            if (curSlide === maxSlide - 1) {
+                curSlide = 0;
+            } else {
+                curSlide++;
+            }
+
+            goToSlide(curSlide);
+            activeDot(curSlide);
+        };
+
+        const prevSlide = function () {
+            if (curSlide === 0) {
+                curSlide = maxSlide - 1;
+            } else {
+                curSlide--;
+            }
+
+            goToSlide(curSlide);
+            activeDot(curSlide);
+        };
+        const init = function () {
+            createDots();
+            goToSlide(0);
+            activeDot(0);
+        };
+
+        init();
+
+        btnLeft.addEventListener('click', prevSlide);
+        btnRight.addEventListener('click', nextSlide);
+
+        document.addEventListener('keydown', function (e) {
+            if (e.key === "ArrowLeft") prevSlide();
+            e.key === "ArrowRight" && nextSlide();
+        });
+
+        dotContainer.addEventListener('click', function (e) {
+            if (e.target.classList.contains('dots-dot')) {
+                const { slide } = e.target.dataset;
+                goToSlide(slide);
+                activeDot(slide);
+            }
+        });
+        setInterval(nextSlide, 6000);
+    };
+
+    slider();
+
+} catch { }
+
+try {
     const inputBox = document.getElementsByTagName('input');
 
     const loginActivate = function (e) {
