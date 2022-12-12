3daba12b08a120ca2991bcef0a61968a633e6bb2
qlghwp123 <qlghwp123@naver.com>
2022-12-13 06:27:53 +0900

    style : Add blank line .models import Todos 부분이 빠졌다 왜인지는 모르겠다.

--- todos/admin.py
+++ todos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Todos
 
+
 # Register your models here.
 class TodosAdmin(admin.ModelAdmin):
     fields = ("user_id", "title", "content", "when", "started_at", "expired_at", "is_completed")
