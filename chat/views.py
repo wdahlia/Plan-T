e7c7dd71ca93547938add370571e5b1001dab886
dahlia <wlstnrdahlia@gmail.com>
2022-12-13 11:48:20 +0900

    templates : chat.html 구조 템플릿 작성

--- chat/views.py
+++ chat/views.py
@@ -10,10 +10,12 @@ def room(request, study_pk):
     user = request.user
     study = Study.objects.get(id=study_pk)
 
+    
     # 로그인 검증 및 스터디원 여부 파악
     if user.is_authenticated:
         if user in study.participated.all() and study in user.join_study.all():
             nickname = user.username
+            memberimg = user.image
         else:
             messages.error(request, "스터디에 가입해주세요.")
 
@@ -24,8 +26,10 @@ def room(request, study_pk):
         return redirect("studies:detail", study_pk)
 
     context = {
+        "study": study,
         "room_id": study_pk,
         "nickname": nickname,
+        "memberimg": memberimg,
     }
 
-    return render(request, "chat/room.html", context)
+    return render(request, "chat/test.html", context)
