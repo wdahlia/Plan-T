fe3b16a590acca3bb464e0b0dcbceb281e5f3fb4
OIIUOI <eunjin4788@gmail.com>
2022-12-13 15:23:33 +0900

    fix : 태그 날아간 코드 복구 태그 날아간 코드 수정

--- todos/tests.py
+++ todos/tests.py
@@ -1,12 +1,1 @@
 from django.test import TestCase
-
-tags = "aan,aa"
-if "," in tags:
-    taglist = set(tags.replace(" ", "").split(","))
-else:
-    taglist = tags.replace(" ", "")
-
-print(taglist)
-# replace 는 걍 둬도 되고 split , 하려고 하는데 , 가 없으면 문제
-a = set(tags)
-print(a)
