9ae49898371a23e6e3892a0c1f75ec4e06b7f5ab
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-12-14 11:53:29 +0900

    style : todos/views/create_tag - create_tag를 함수로 만들어서 관리

--- function.py
+++ function.py
@@ -2,8 +2,19 @@ def change_value(value):
     if value == "":
         index = value
         print("공백이 들어옴")
-
     else:
         hour, min = map(int, value.split(":"))
         index = ((hour - 6) * 6) + (min // 10)
         return int(index)
+
+
+def create_tag(tags, Tag, todo):
+    if tags != "":
+        tags_ = tags.replace(" ", "")
+        if "," in tags_:
+            for tag in tags.split(",")[:5]:
+                tag_ = tag.replace(" ", "")
+                if tag_ != "":
+                    Tag.objects.create(todo=todo, content=tag_)
+        else:
+            Tag.objects.create(todo=todo, content=tags_)
